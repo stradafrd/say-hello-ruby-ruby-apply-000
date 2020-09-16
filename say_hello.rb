156404bb1cb06218964f356db1e09dfdfc0bb50d
Fredy Estrada <stradafrd234@gmail.com>
2020-09-16 03:57:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,4 @@
-def say_hello(name= "Ruby Programmer")
-  puts "Hello, #{name}"
+def say_hello(name="Ruby Programmer")
+  puts "Hello " + name + "!"
 end
-
-puts say_hello("Gabriela")
+say_hello("Gabriela")
